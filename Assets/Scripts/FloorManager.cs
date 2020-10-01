04ee21dd6e3ef0178f52d023a44137975042ff90
Evan Dimopoulos <evan.dime@gmail.com>
2020-10-01 15:53:18 -0400

    Scripts that generate rooms

--- Assets/Scripts/FloorManager.cs
+++ Assets/Scripts/FloorManager.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class FloorManager : MonoBehaviour
 {
+    public RoomManager roomScript;
+
+    private int floor = 6;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        roomScript = GetComponent<RoomManager>();
+        InitFloor();
+    }
+
+    void InitFloor()
+    {
+        roomScript.SetupRoom(1,floor);
     }
 
     // Update is called once per frame
