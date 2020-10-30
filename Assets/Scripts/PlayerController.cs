f1d7be8340e8ad11f78660920396deed10e9d138
scott <scottjamessmith05@gmail.com>
2020-10-29 20:06:42 -0400

    merge fixes and convert back to old input manager

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.LowLevel;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,106 +9,45 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D m_rigidbody = default;
     private Animator m_animator = default;
-    private Keyboard keyboard;
-    private Mouse mouse;
-    private CurrentGamepad currentGamepad;
 
     private const string k_attackAnim = "Attack";
+
+    private const string k_fireButton = "Fire";
+    private const string k_horizontalAxis = "Horizontal";
+    private const string k_verticalAxis = "Vertical";
 
     void Start()
     {
         m_rigidbody = GetComponent<Rigidbody2D>();
         m_animator = GetComponent<Animator>();
-        currentGamepad = new CurrentGamepad();
     }
 
     void Update()
     {
-        keyboard = Keyboard.current;
-        mouse = Mouse.current;
         UpdateCharacterStates();
     }
 
     private void UpdateCharacterStates()
     {
-        if (keyboard == null || mouse == null)
-            return;
-
         Walk();
         Attack();
     }
 
     private void Walk()
     {
-        Vector2 wasd = new Vector2(-Convert.ToInt16(keyboard.aKey.isPressed) + Convert.ToInt16(keyboard.dKey.isPressed),
-            Convert.ToInt16(keyboard.wKey.isPressed) - Convert.ToInt16(keyboard.sKey.isPressed));
+        Vector2 walkVector = new Vector2(Input.GetAxisRaw(k_horizontalAxis), Input.GetAxisRaw(k_verticalAxis));
 
-        Vector2 walk = (currentGamepad.LeftStick().magnitude > 0 ? currentGamepad.LeftStick() : wasd).normalized * m_walkSpeed;
+        if (walkVector.magnitude > 1)
+            walkVector = walkVector.normalized;
 
-        transform.Translate(walk * Time.deltaTime);
+        transform.Translate(walkVector * m_walkSpeed * Time.deltaTime);
     }
 
     private void Attack()
     {
-        if(mouse.leftButton.IsPressed() || currentGamepad.IsPressed(GamepadButton.West))
+        if (Input.GetButton(k_fireButton))
         {
             m_animator.SetTrigger(k_attackAnim);
         }
     }
-
-    private class CurrentGamepad
-    {
-        private Gamepad gamepad = default;
-
-        public CurrentGamepad()
-        {
-            InputSystem.onDeviceChange +=
-                (device, change) =>
-                {
-                    switch (change)
-                    {
-                        case InputDeviceChange.Added:
-                            gamepad = Gamepad.current;
-                            UnityEngine.Debug.Log($"Device {device} was added");
-                            break;
-                        case InputDeviceChange.Removed:
-                            gamepad = null;
-                            UnityEngine.Debug.Log($"Device {device} was removed");
-                            break;
-                    }
-                };
-
-            gamepad = Gamepad.current;
-        }
-
-        public Vector2 LeftStick()
-        {
-            if (gamepad != null)
-            {
-                return gamepad.leftStick.ReadValue();
-            }
-            return Vector2.zero;
-        }
-
-        public bool IsPressed(GamepadButton button)
-        {
-            if (gamepad != null)
-            {
-                return gamepad[button].isPressed;
-            }
-
-            return false;
-        }
-
-        public bool WasReleased(GamepadButton button)
-        {
-            if (gamepad != null)
-            {
-                return gamepad[button].wasReleasedThisFrame;
-            }
-
-            return false;
-        }
-    }
-
 }
