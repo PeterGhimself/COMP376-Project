c129436d03da3368bfdc4d465b8d52961445c0cf
Evan Dimopoulos <evan.dime@gmail.com>
2020-11-12 15:56:51 -0500

    Added fire script to obstacles. Renamed hurter and blocker to rock and fire

---
+++ Assets/Scripts/Obstacles/Fire.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : Obstacle
+{
+    // Start is called before the first frame update
+    [SerializeField] public float Damage;
+    [SerializeField] private LayerMask m_owlLayers = default;
+
+    void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (m_owlLayers == (m_owlLayers | (1 << other.gameObject.layer)))
+        {
+            Owl owl = other.gameObject.GetComponent<Owl>();
+            if (owl)
+            {
+                owl.ApplyDamage(Damage);
+            }
+            else
+            {
+                Debug.LogError("No owl script on " + owl.gameObject.name);
+            }
+        }else if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerController player = other.gameObject.GetComponent<PlayerController>();
+            if (player)
+            {
+                player.DamagePlayer(Damage);
+            }
+            else
+            {
+                Debug.LogError("No player script on " + player.gameObject.name);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Obstacles/Obstacle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    //Will be used if ever we need something for all obstacles
+    private RoomManager _mRoomManager;
+
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        _mRoomManager = gameObject.transform.parent.GetComponent<RoomManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
