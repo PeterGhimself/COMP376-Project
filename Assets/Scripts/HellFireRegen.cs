62f24d1dbec61c0d3026430bb7e7e71be51e436d
Evan Dimopoulos <evan.dime@gmail.com>
2020-11-17 15:30:47 -0500

    Added hellfire that is just a fire that regens in size if not destroyed

---
+++ Assets/Scripts/HellFireRegen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HellFireRegen : MonoBehaviour
+{
+    private ObstacleTakeDamage obstacleScript;
+    private float deltaDamaged = 0;
+    [SerializeField]private float regenTime = 2.5f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        obstacleScript = gameObject.GetComponent<ObstacleTakeDamage>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Math.Abs(obstacleScript.hitPoints - obstacleScript.maxHitPoints) > 0)
+        {
+            deltaDamaged += Time.deltaTime;
+        }
+        else
+        {
+            deltaDamaged = 0;
+        }
+
+        if (deltaDamaged >= regenTime)
+        {
+            obstacleScript.hitPoints += 1;
+            deltaDamaged = 0;
+        }
+    }
+}
