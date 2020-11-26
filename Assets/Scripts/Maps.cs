a690700ea751e11fffcc4e844c6762532a4da7ca
JonathanSolvesProblems <jon_andrei04@hotmail.com>
2020-11-26 12:42:00 -0500

    working on changing map prefabs, based on level.

---
+++ Assets/Scripts/Maps.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maps : MonoBehaviour
+{
+
+    [SerializeField] [Range(1,4)] // depending on level. Will change prefab color.
+    int m_floor = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_floor == 1) 
+        {
+            
+        } 
+        else if(m_floor == 2)
+        {
+
+        } 
+        else if (m_floor == 3)
+        {
+
+        }
+        else if (m_floor == 4)
+        {
+
+        }
+    }
+}
