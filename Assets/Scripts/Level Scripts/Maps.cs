726ec90913d0a6933e0a70be52237630ae54122c
JonathanSolvesProblems <jon_andrei04@hotmail.com>
2020-11-26 13:10:47 -0500

    instantiated test worked, minor bug. Working on fixing.

--- Assets/Scripts/Level Scripts/Maps.cs
+++ Assets/Scripts/Level Scripts/Maps.cs
@@ -8,12 +8,23 @@ public class Maps : MonoBehaviour
     [SerializeField] [Range(1,4)] // depending on level. Will change prefab color.
     int m_floor = 1;
 
+    public GameObject prefabRef;
+
+    GameObject prefabInstantiated;
+    
+
+
+    void Start() 
+    {
+        prefabInstantiated = Instantiate(prefabRef, new Vector2(0, 0), Quaternion.identity);
+    }
+
     // Update is called once per frame
     void Update()
     {
         if(m_floor == 1) 
         {
-            
+            prefabInstantiated.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.red);
         } 
         else if(m_floor == 2)
         {
@@ -27,5 +38,6 @@ public class Maps : MonoBehaviour
         {
 
         }
+        Destroy(prefabInstantiated);
     }
 }
