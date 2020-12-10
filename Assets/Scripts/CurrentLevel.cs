395ce99eefe1de1532c59eb934f76e0aa3dc7589
PeterGhimself <petergranitski@gmail.com>
2020-12-09 23:42:54 -0500

    [#48] Add current level top right corner

---
+++ Assets/Scripts/CurrentLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentLevel : MonoBehaviour
+{
+    private GameManager gameManager;
+    public Text levelText;
+
+    void Start()
+    {
+        this.gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int currentLevel = gameManager.getLevel();
+        levelText.text = "Level: " + (currentLevel);
+    }
+}
