f4c4f1681a83399d9f0dbaa1a3f83d886965da5d
PeterGhimself <petergranitski@gmail.com>
2020-12-11 00:19:24 -0500

    [#46] Setup AudioManager, add GameMenu background music

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+// easily add/remove sounds as we go
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    void Awake ()
+    {
+        foreach (Sound s in sounds)
+        {
+           s.source = gameObject.AddComponent<AudioSource>();
+           s.source.clip = s.clip;
+           s.source.volume = s.volume;
+           s.source.pitch = s.pitch;
+           s.source.loop = s.loop;
+        }
+    }
+
+    public void Play (string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            print("WARNING: sound with name " + name + " not found!");
+            return;
+        }
+
+        s.source.Play();
+
+    }
+}
