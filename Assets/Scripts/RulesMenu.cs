513ffc27cceafcaedc098a069460f8020807465b
Rainily <reynald.servera@hotmail.com>
2020-12-11 00:37:53 -0500

    New Controls Menu + README update
    - revamped the controls menu
    - added sources for third party sprites in README

---
+++ Assets/Scripts/RulesMenu.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RulesMenu : MonoBehaviour
+{
+    public GameObject basicControlPanel;
+    public GameObject playerPanel;
+    public GameObject enemiesPanel;
+    public GameObject bossesPanel;
+    public GameObject weaponsPanel;
+    public GameObject abilitiesPanel;
+    public GameObject powerUpsPanel;
+
+    public void showBasicControlsPanel()
+    {
+        basicControlPanel.SetActive(true);
+        playerPanel.SetActive(false);
+        enemiesPanel.SetActive(false);
+        bossesPanel.SetActive(false);
+        weaponsPanel.SetActive(false);
+        abilitiesPanel.SetActive(false);
+        powerUpsPanel.SetActive(false);
+    }
+
+    public void showPlayerPanel()
+    {
+        basicControlPanel.SetActive(false);
+        playerPanel.SetActive(true);
+        enemiesPanel.SetActive(false);
+        bossesPanel.SetActive(false);
+        weaponsPanel.SetActive(false);
+        abilitiesPanel.SetActive(false);
+        powerUpsPanel.SetActive(false);
+    }
+
+    public void showEnemiesPanel()
+    {
+        basicControlPanel.SetActive(false);
+        playerPanel.SetActive(false);
+        enemiesPanel.SetActive(true);
+        bossesPanel.SetActive(false);
+        weaponsPanel.SetActive(false);
+        abilitiesPanel.SetActive(false);
+        powerUpsPanel.SetActive(false);
+    }
+
+    public void showBossesPanel()
+    {
+        basicControlPanel.SetActive(false);
+        playerPanel.SetActive(false);
+        enemiesPanel.SetActive(false);
+        bossesPanel.SetActive(true);
+        weaponsPanel.SetActive(false);
+        abilitiesPanel.SetActive(false);
+        powerUpsPanel.SetActive(false);
+    }
+
+    public void showWeaponsPanel()
+    {
+        basicControlPanel.SetActive(false);
+        playerPanel.SetActive(false);
+        enemiesPanel.SetActive(false);
+        bossesPanel.SetActive(false);
+        weaponsPanel.SetActive(true);
+        abilitiesPanel.SetActive(false);
+        powerUpsPanel.SetActive(false);
+    }
+
+    public void showAbilitiesPanel()
+    {
+        basicControlPanel.SetActive(false);
+        playerPanel.SetActive(false);
+        enemiesPanel.SetActive(false);
+        bossesPanel.SetActive(false);
+        weaponsPanel.SetActive(false);
+        abilitiesPanel.SetActive(true);
+        powerUpsPanel.SetActive(false);
+    }
+
+    public void showPowerUpsPanel()
+    {
+        basicControlPanel.SetActive(false);
+        playerPanel.SetActive(false);
+        enemiesPanel.SetActive(false);
+        bossesPanel.SetActive(false);
+        weaponsPanel.SetActive(false);
+        abilitiesPanel.SetActive(false);
+        powerUpsPanel.SetActive(true);
+    }
+}
